c16e67b97a2f30317646f47dfcc570a0a971e0e0
doofin <8177dph@gmail.com>
2018-07-21 17:19:26 +0800

    add stack

--- src/IRTS/CodegenEmpty.hs
+++ src/IRTS/CodegenEmpty.hs
@@ -1,4 +1,30 @@
-module IRTS.CodegenEmpty(codegenEmpty) where
+module IRTS.CodegenEmpty (codegenEmpty) where
+
+{-
+module IRTS.CodegenCommon -- at package idris 
+
+type CodeGenerator = CodegenInfo -> IO ()
+
+data CodegenInfo = CodegenInfo {
+    outputFile    :: String
+  , outputType    :: OutputType
+  , targetTriple  :: String
+  , targetCPU     :: String
+  , includes      :: [FilePath]
+  , importDirs    :: [FilePath]
+  , compileObjs   :: [String]
+  , compileLibs   :: [String]
+  , compilerFlags :: [String]
+  , debugLevel    :: DbgLevel
+  , simpleDecls   :: [(Name, SDecl)]
+  , defunDecls    :: [(Name, DDecl)]
+  , liftDecls     :: [(Name, LDecl)]
+  , interfaces    :: Bool
+  , exportDecls   :: [ExportIFace]
+  , ttDecls       :: [(Name, TTDecl)]
+  }
+
+-}
 
 import IRTS.CodegenCommon
 
